1d53596f8b072cf9e89a1b1344033725dc59ec27
Harrison Hayes <harrisonayes97@yahoo.com>
2019-01-04 23:29:47 -0600

    updated folder, added manager

---
+++ manager/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+import Router from'./Router';
+
+class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyDxOKjrTlnKoKsRBopC61M8TyDIoT3qYgs',
+            authDomain: 'manager-8a24f.firebaseapp.com',
+            databaseURL: 'https://manager-8a24f.firebaseio.com',
+            projectId: 'manager-8a24f',
+            storageBucket: 'manager-8a24f.appspot.com',
+            messagingSenderId: '973997531960'
+          };
+          firebase.initializeApp(config);
+    }
+
+
+    render() {
+
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+        return (
+            <Provider store={store}>
+                <LoginForm />
+            </Provider>
+        );
+    }
+
+}
+
+export default App;
